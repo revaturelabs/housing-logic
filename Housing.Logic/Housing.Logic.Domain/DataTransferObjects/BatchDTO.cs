78c0471c132ab1b8d2d7d44dc3e775d61a1191db
raykonopka <konopka.ray@gmail.com>
2017-01-31 10:26:42 -0500

    add DTOs

---
+++ Housing.Logic/Housing.Logic.Domain/DataTransferObjects/BatchDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Housing.Logic.Domain.DataTransferObjects
+{
+    public class BatchDTO
+    {
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public string Technology { get; set; }
+    }
+}
---
+++ Housing.Logic/Housing.Logic.Domain/DataTransferObjects/HousingDataDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Housing.Logic.Domain.DataTransferObjects
+{
+    public class HousingDataDTO
+    {
+        public string AssociateEmail { get; set; }
+        public string HousingUnitName { get; set; }
+        public System.DateTime MoveInDate { get; set; }
+        public System.DateTime MoveOutDate { get; set; }
+        public string HousingDataAltId { get; set; }
+    }
+}
