dc59a9a3e3b9130894d99eae4999d8a099fcb912
jason goodell <jason.goodell@outlook.com>
2017-02-14 16:45:32 -0500

    work on endpoint testing/debugging

--- Housing.Logic/Housing.Logic.Domain/DataAccess.cs
+++ Housing.Logic/Housing.Logic.Domain/DataAccess.cs
@@ -17,7 +17,8 @@ namespace Housing.Logic.Domain
     public class DataAccess
     {
 
-        const string apiURL = "http://34.193.92.187/housingdata/api/";
+        //const string apiURL = "http://34.193.92.187/housingdata/api/";
+        const string apiURL = "http://localhost:57200/api/";
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
 
